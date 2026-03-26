e9b974d0bc3fae75ccbbe3251968c91bcfc94ff8
Jose Melgares <roberto.melgares@ct.uneatlantico.es>
2026-03-26 09:41:30 +0100

    feat(admin): adds admin dashboard menu to edit modals

---
+++ includes/amoeba-dashboard.php
@@ -0,0 +1,119 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Class Amoeba_Dashboard
+ * Handles the list view of all created modals.
+ */
+class Amoeba_Dashboard {
+
+    /**
+     * Amoeba_Dashboard constructor.
+     */
+    public function __construct() {
+        add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+    }
+
+    /**
+     * Adds the admin menu pages.
+     */
+    public function add_admin_menu() {
+        // Main Menu: Dashboard / List
+        add_menu_page(
+            __( 'Amoeba Modals', 'custom-modal-creator' ),
+            __( 'Amoeba Modals', 'custom-modal-creator' ),
+            'manage_options',
+            'amoeba-modals',
+            array( $this, 'render_dashboard_page' ),
+            'dashicons-format-image',
+            60
+        );
+
+        // Submenu: All Modals (duplicate of main for clarity)
+        add_submenu_page(
+            'amoeba-modals',
+            __( 'All Modals', 'custom-modal-creator' ),
+            __( 'All Modals', 'custom-modal-creator' ),
+            'manage_options',
+            'amoeba-modals',
+            array( $this, 'render_dashboard_page' )
+        );
+
+        // Submenu: Add New
+        add_submenu_page(
+            'amoeba-modals',
+            __( 'Add New Modal', 'custom-modal-creator' ),
+            __( 'Add New', 'custom-modal-creator' ),
+            'manage_options',
+            'amoeba-settings', // Points to the editor class slug
+            null 
+        );
+    }
+
+    /**
+     * Renders the dashboard page with the table of modals.
+     */
+    public function render_dashboard_page() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'amoeba_modals';
+        $modals = $wpdb->get_results( "SELECT * FROM $table_name ORDER BY id DESC" );
+
+        // Handle deletions
+        if ( isset( $_GET['action'] ) && $_GET['action'] === 'delete' && isset( $_GET['id'] ) ) {
+            check_admin_referer( 'delete_modal_' . $_GET['id'] );
+            $wpdb->delete( $table_name, array( 'id' => intval( $_GET['id'] ) ) );
+            echo '<div class="updated"><p>' . __( 'Modal deleted.', 'custom-modal-creator' ) . '</p></div>';
+            // Refresh list
+            $modals = $wpdb->get_results( "SELECT * FROM $table_name ORDER BY id DESC" );
+        }
+
+        ?>
+        <div class="wrap">
+            <h1 class="wp-heading-inline"><?php _e( 'Amoeba Modals', 'custom-modal-creator' ); ?></h1>
+            <a href="<?php echo admin_url( 'admin.php?page=amoeba-settings' ); ?>" class="page-title-action"><?php _e( 'Add New', 'custom-modal-creator' ); ?></a>
+            <hr class="wp-header-end">
+
+            <table class="wp-list-table widefat fixed striped">
+                <thead>
+                    <tr>
+                        <th scope="col" class="column-id" style="width: 50px;"><?php _e( 'ID', 'custom-modal-creator' ); ?></th>
+                        <th scope="col"><?php _e( 'Title', 'custom-modal-creator' ); ?></th>
+                        <th scope="col"><?php _e( 'Shortcode', 'custom-modal-creator' ); ?></th>
+                        <th scope="col"><?php _e( 'Color', 'custom-modal-creator' ); ?></th>
+                        <th scope="col"><?php _e( 'Actions', 'custom-modal-creator' ); ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if ( $modals ) : ?>
+                        <?php foreach ( $modals as $modal ) : ?>
+                            <tr>
+                                <td><?php echo esc_html( $modal->id ); ?></td>
+                                tube
+                                <td><strong><a href="<?php echo admin_url( 'admin.php?page=amoeba-settings&id=' . $modal->id ); ?>"><?php echo esc_html( $modal->title ); ?></a></strong></td>
+                                <td><code>[amoeba_modal id="<?php echo esc_html( $modal->id ); ?>"]</code></td>
+                                <td>
+                                    <span style="display:inline-block; width: 20px; height: 20px; background: <?php echo esc_attr( $modal->hex_color ); ?>; border: 1px solid #ccc; vertical-align: middle; margin-right: 5px;"></span>
+                                    <?php echo esc_html( $modal->hex_color ); ?>
+                                </td>
+                                <td>
+                                    <a href="<?php echo admin_url( 'admin.php?page=amoeba-settings&id=' . $modal->id ); ?>" class="button button-small"><?php _e( 'Edit', 'custom-modal-creator' ); ?></a>
+                                    <a href="<?php echo wp_nonce_url( admin_url( 'admin.php?page=amoeba-modals&action=delete&id=' . $modal->id ), 'delete_modal_' . $modal->id ); ?>" class="button button-small button-link-delete" onclick="return confirm('<?php _e( 'Are you sure?', 'custom-modal-creator' ); ?>')"><?php _e( 'Delete', 'custom-modal-creator' ); ?></a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <tr>
+                            <td colspan="5"><?php _e( 'No modals found. Create your first one!', 'custom-modal-creator' ); ?></td>
+                        </tr>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+    }
+}
+
+new Amoeba_Dashboard();
---
+++ includes/amoeba-settings.php
@@ -0,0 +1,139 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Class Amoeba_Settings
+ * Handles the editing and creation of modals in the DB.
+ */
+class Amoeba_Settings {
+
+    /**
+     * Amoeba_Settings constructor.
+     */
+    public function __construct() {
+        add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+        add_action( 'admin_init', array( $this, 'handle_save' ) );
+    }
+
+    /**
+     * Adds a hidden/submenu page for editing.
+     */
+    public function add_admin_menu() {
+        // This page is reached via the Dashboard Edit button or "Add New"
+        add_submenu_page(
+            null, // Hidden from menu, handled by Dashboard or specific links
+            __( 'Edit Modal', 'custom-modal-creator' ),
+            __( 'Edit Modal', 'custom-modal-creator' ),
+            'manage_options',
+            'amoeba-settings',
+            array( $this, 'render_settings_page' )
+        );
+    }
+
+    /**
+     * Handles the POST request to save modal data.
+     */
+    public function handle_save() {
+        if ( ! isset( $_POST['amoeba_save_modal'] ) || ! check_admin_referer( 'amoeba_save_modal_action' ) ) {
+            return;
+        }
+
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'amoeba_modals';
+
+        $id = isset( $_POST['modal_id'] ) ? intval( $_POST['modal_id'] ) : 0;
+        $data = array(
+            'title'       => sanitize_text_field( $_POST['title'] ),
+            'content'     => wp_kses_post( $_POST['content'] ),
+            'picture_url' => esc_url_raw( $_POST['picture_url'] ),
+            'hex_color'   => sanitize_hex_color( $_POST['hex_color'] ),
+            'custom_css'  => wp_strip_all_tags( $_POST['custom_css'] ),
+        );
+
+        if ( $id > 0 ) {
+            $wpdb->update( $table_name, $data, array( 'id' => $id ) );
+            $message = 'updated';
+        } else {
+            $wpdb->insert( $table_name, $data );
+            $id = $wpdb->insert_id;
+            $message = 'created';
+        }
+
+        wp_redirect( admin_url( 'admin.php?page=amoeba-settings&id=' . $id . '&message=' . $message ) );
+        exit;
+    }
+
+    /**
+     * Renders the editor page.
+     */
+    public function render_settings_page() {
+        global $wpdb;
+        $id = isset( $_GET['id'] ) ? intval( $_GET['id'] ) : 0;
+        $modal = null;
+
+        if ( $id > 0 ) {
+            $table_name = $wpdb->prefix . 'amoeba_modals';
+            $modal = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $table_name WHERE id = %d", $id ) );
+        }
+
+        $title = $modal ? $modal->title : '';
+        $content = $modal ? $modal->content : '';
+        $picture_url = $modal ? $modal->picture_url : '';
+        $hex_color = $modal ? $modal->hex_color : '#333333';
+        $custom_css = $modal ? $modal->custom_css : '';
+
+        ?>
+        <div class="wrap">
+            <h1><?php echo $id > 0 ? __( 'Edit Modal', 'custom-modal-creator' ) : __( 'Add New Modal', 'custom-modal-creator' ); ?></h1>
+            
+            <?php if ( isset( $_GET['message'] ) ) : ?>
+                <div class="updated"><p><?php echo $_GET['message'] === 'updated' ? __( 'Modal updated.', 'custom-modal-creator' ) : __( 'Modal created.', 'custom-modal-creator' ); ?></p></div>
+            <?php endif; ?>
+
+            <form method="post" action="">
+                <?php wp_nonce_field( 'amoeba_save_modal_action' ); ?>
+                <input type="hidden" name="modal_id" value="<?php echo esc_attr( $id ); ?>">
+                
+                <table class="form-table">
+                    <tr>
+                        <th scope="row"><label for="title"><?php _e( 'Title', 'custom-modal-creator' ); ?></label></th>
+                        <td><input name="title" type="text" id="title" value="<?php echo esc_attr( $title ); ?>" class="regular-text" required></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="content"><?php _e( 'Content', 'custom-modal-creator' ); ?></label></th>
+                        <td><textarea name="content" id="content" rows="10" cols="50" class="large-text"><?php echo esc_textarea( $content ); ?></textarea></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="picture_url"><?php _e( 'Picture URL', 'custom-modal-creator' ); ?></label></th>
+                        <td><input name="picture_url" type="text" id="picture_url" value="<?php echo esc_attr( $picture_url ); ?>" class="regular-text"></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="hex_color"><?php _e( 'Hex Color', 'custom-modal-creator' ); ?></label></th>
+                        <td><input name="hex_color" type="text" id="hex_color" value="<?php echo esc_attr( $hex_color ); ?>" class="regular-text" placeholder="#ffffff"></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="custom_css"><?php _e( 'Custom CSS', 'custom-modal-creator' ); ?></label></th>
+                        <td><textarea name="custom_css" id="custom_css" rows="5" cols="50" class="large-text"><?php echo esc_textarea( $custom_css ); ?></textarea></td>
+                    </tr>
+                    <?php if ( $id > 0 ) : ?>
+                    <tr>
+                        <th scope="row"><?php _e( 'Shortcode', 'custom-modal-creator' ); ?></th>
+                        <td><code>[amoeba_modal id="<?php echo esc_attr( $id ); ?>"]</code></td>
+                    </tr>
+                    <?php endif; ?>
+                </table>
+
+                <p class="submit">
+                    <input type="submit" name="amoeba_save_modal" id="submit" class="button button-primary" value="<?php _e( 'Save Modal', 'custom-modal-creator' ); ?>">
+                    <a href="<?php echo admin_url( 'admin.php?page=amoeba-modals' ); ?>" class="button"><?php _e( 'Back to List', 'custom-modal-creator' ); ?></a>
+                </p>
+            </form>
+        </div>
+        <?php
+    }
+}
+
+new Amoeba_Settings();
