da152997ce023f2c2acda8ff954d443b84813ab9
Jose Melgares <roberto.melgares@ct.uneatlantico.es>
2026-03-26 08:48:19 +0100

    feat: initial plugin files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Amoeba - Modal Creator
+
+A simple custom modal creator plugin for your WordPress sites via shortcode. 
+
+By: 🐌
---
+++ amoeba-modal.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Amoeba - Custom Modal Creator
+ * Description: Easily create modals for your WordPress site plugin.
+ * Version: 1.0.0
+ * Author: Jose Melgares
+ * Text Domain: custom-modal-creator
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
